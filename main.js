672bb24de1ba81202afeb320ef8d57868b1402e8
ErikaGalanCont <102776229+ErikaGalanCont@users.noreply.github.com>
2023-03-15 20:18:33 -0500

    Mediante atributos

---
+++ main.js
@@ -0,0 +1,21 @@
+// let btn = document.querySelector("#btn")
+// let input = document.querySelector("#myinput")
+
+// input.addEventListener("keypress")
+
+
+
+// btn.addEventListener("click", (e)=>{
+//     console.log("Hice click");
+// })
+// btn.addEventListener("mouseover", (e)=>{
+//     console.log("Estoy encima del boton");
+// })
+// btn.addEventListener("mouseout", (e)=>{
+//     console.log("Sali del boton");
+// })
+// btn.addEventListener("focus", (e)=>{
+//     console.log("Sali del boton");
+// })
+
+
